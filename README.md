fa5587aeea1081bc49d747574edb7aeef30613a8
Jakob Stechow <jakob.stechow@pm.me>
2025-06-03 14:38:30 +0200

    feat: learn-bevy with tutorial Episode 1-4

---
+++ README.md
@@ -0,0 +1,9 @@
+# learn-bevy
+
+This repo contains the code from following along a bevy (a rust game engine) tutorial.
+
+Thanks to https://www.youtube.com/@jacques-dev for making a great tutorial!
+
+A link to the tutorial: https://www.youtube.com/watch?v=TQt-v_bFdao&list=PLVnntJRoP85JHGX7rGDu6LaF3fmDDbqyd&pp=0gcJCWMEOCosWNin
+
+Note that the tutorial uses bevy v0.10, but I've used the newest version that was available at the point of following along the tutorial, which is v0.16, so some code was adapted to reflect the breaking changes.
---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Dev environment for some-game-with-bevy";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "alsa-udev-shell";
+          buildInputs = [
+            pkgs.alsa-lib
+            pkgs.systemd
+            pkgs.pkg-config
+          ];
+          LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${
+            with pkgs;
+              pkgs.lib.makeLibraryPath [
+                xorg.libX11
+                xorg.libXcursor
+                xorg.libXi
+                libxkbcommon
+                xorg.libxcb
+                vulkan-loader
+                glfw
+              ]
+          }";
+        };
+      }
+    );
+}
