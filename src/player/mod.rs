630e8507ef2f10ba33a75b0bebbb5fb0ba100622
Jakob Stechow <jakob.stechow@pm.me>
2025-06-06 22:28:48 +0200

    feat: episode 8 - explicit ordering and system sets

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -10,14 +10,20 @@ pub const PLAYER_SPRITE_SRC: &str = "sprites/ball_blue_large.png";
 pub const PLAYER_SPRITE_SIZE: f32 = 64.0;
 pub const PLAYER_SPEED: f32 = 500.0;
 
-pub struct PlayerPlugin {}
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ConfinementSystemSet;
+
+pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player)
-            .add_systems(Update, player_movement)
-            .add_systems(Update, confine_player_movement)
-            .add_systems(Update, enemy_hit_player)
-            .add_systems(Update, player_hit_star);
+        app.configure_sets(Update, MovementSystemSet.before(ConfinementSystemSet))
+            .add_systems(Startup, spawn_player)
+            .add_systems(Update, player_movement.in_set(MovementSystemSet))
+            .add_systems(Update, confine_player_movement.in_set(ConfinementSystemSet))
+            .add_systems(Update, (enemy_hit_player, player_hit_star));
     }
 }
